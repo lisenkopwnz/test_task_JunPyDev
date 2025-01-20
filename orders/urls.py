3ca43887b313fc0b216dc7f480dae55c68d150e2
Tolik pwmz <pal3noa@ya.ru>
2025-01-20 14:44:45 +0300

    добавил эндпоинт для отображения заказов подключил urls

---
+++ orders/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from orders.views import OrderListView
+
+urlpatterns = [
+    path('', OrderListView.as_view()),
+]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from django.views.generic import ListView
 
-# Create your views here.
+from orders.models import Order, OrderDish, Dish
+
+
+class OrderListView(ListView):
+    """
+        Представление для отображения списка заказов на главной странице.
+    """
+    template_name = 'orders/order_list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        """
+            Возвращает queryset заказов с предварительной загрузкой связанных данных.
+
+            Использует:
+                - `prefetch_related` для загрузки связанных объектов OrderDish и Dish.
+                - `only` для выбора только необходимых полей.
+
+            Возвращает:
+                QuerySet: Список заказов с предварительно загруженными данными о блюдах и их количестве.
+        """
+        orders = Order.objects.prefetch_related(
+            Prefetch('order_dishes', queryset=OrderDish.objects.only('quantity', 'price_at_order', 'dish')),
+            Prefetch('order_dishes__dish', queryset=Dish.objects.only('name', 'price')),
+        ).only('id', 'table_number', 'status')
+        return orders
