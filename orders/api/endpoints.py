0aa0a3677fbc68e81265cad8090ce17e527affb4
Tolik pwmz <pal3noa@ya.ru>
2025-01-21 20:09:18 +0300

    добавил добавил Api эндпоинт который детали заказа

--- orders/api/endpoints.py
+++ orders/api/endpoints.py
@@ -1,6 +1,6 @@
 from django.db.models import Prefetch
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from orders.api.filters import OrderFilter
 from orders.api.pagination import OrderPagination
@@ -8,13 +8,7 @@ from orders.api.serializers import OrderSerializer
 from orders.models import Order, OrderDish, Dish
 
 
-class ApiOrderList(ListAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    pagination_class = OrderPagination
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = OrderFilter
-
+class OrderQuerysetMixin:
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.prefetch_related(
@@ -22,3 +16,17 @@ class ApiOrderList(ListAPIView):
             Prefetch('order_dishes__dish', queryset=Dish.objects.only('name', 'price')),
         ).only('id', 'table_number', 'status')
         return queryset
+
+class ApiOrderList(OrderQuerysetMixin,ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    pagination_class = OrderPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = OrderFilter
+
+class ApiOrderDetail(OrderQuerysetMixin,RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    lookup_field = 'id'
+
+
--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .api.endpoints import ApiOrderList
+from .api.endpoints import ApiOrderList, ApiOrderDetail
 from .views import OrderListView, CreateOrder, DeleteOrder, UpdateOrderStatus
 
 app_name = 'orders'
@@ -11,5 +11,6 @@ urlpatterns = [
     path('delete_order/<int:pk>/', DeleteOrder.as_view(), name='delete_order'),  # Исправлено
     path('update-status/<int:order_id>/', UpdateOrderStatus.as_view(), name='update_status'),
 
-    path('api/order_list', ApiOrderList.as_view(), name='api_order_list')
+    path('api/order_list', ApiOrderList.as_view(), name='api_order_list'),
+    path('api/order/<int:id>/', ApiOrderDetail.as_view(), name='order-detail'),
 ]
